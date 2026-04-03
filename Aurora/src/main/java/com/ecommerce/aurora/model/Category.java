f4dc4a6b6b63d75df3e017fba2622a7508b59a5b
LD-RW <basheermazari4@gmail.com>
2026-04-03 12:23:25 +0300

    Used lombok to reduce boilerplate code

--- Aurora/src/main/java/com/ecommerce/aurora/model/Category.java
+++ Aurora/src/main/java/com/ecommerce/aurora/model/Category.java
@@ -4,36 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
 
-    public Category(String categoryName, Long categoryId) {
-        this.categoryName = categoryName;
-        this.categoryId = categoryId;
-    }
-
-    public Category() {
-
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String name) {
-        this.categoryName = name;
-    }
-
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Long id) {
-        this.categoryId = id;
-    }
 }
