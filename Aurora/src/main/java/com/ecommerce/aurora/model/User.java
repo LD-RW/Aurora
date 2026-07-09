68c8c33eda57c48eb1c84cc3997fb9f570d88f3e
LD-RW <basheermazari4@gmail.com>
2026-07-09 18:03:15 +0300

    feat(auth): implement user and role database relations

--- Aurora/src/main/java/com/ecommerce/aurora/model/User.java
+++ Aurora/src/main/java/com/ecommerce/aurora/model/User.java
@@ -6,7 +6,12 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @NoArgsConstructor
@@ -36,4 +41,11 @@ public class User {
     @Email
     private String email;
 
+    @Getter
+    @Setter
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE} ,fetch = FetchType.LAZY)
+    @JoinTable(name = "user_role",
+                joinColumns = @JoinColumn(name = "user_id"),
+                inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles =  new HashSet<>();
 }
