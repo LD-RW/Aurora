7de8642ac58976e51e6f70c193d48bfb9e82d402
LD-RW <basheermazari4@gmail.com>
2026-07-11 16:25:14 +0300

    refactor:add getters and setters for user model collections

--- Aurora/src/main/java/com/ecommerce/aurora/model/User.java
+++ Aurora/src/main/java/com/ecommerce/aurora/model/User.java
@@ -53,12 +53,16 @@ public class User {
     private String email;
 
 
+    @Getter
+    @Setter
     @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE} ,fetch = FetchType.LAZY)
     @JoinTable(name = "user_role",
                 joinColumns = @JoinColumn(name = "user_id"),
                 inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles =  new HashSet<>();
 
+    @Getter
+    @Setter
     @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE} ,fetch = FetchType.LAZY)
     @JoinTable(name = "user_address",
             joinColumns = @JoinColumn(name = "user_id"),
