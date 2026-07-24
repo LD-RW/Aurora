9357d9277ba6331d564707a8bcff856ec6c3f5e8
LD-RW <basheermazari4@gmail.com>
2026-07-24 15:09:30 +0300

    feat(user): add UserRepository interface

---
+++ Aurora/src/main/java/com/ecommerce/aurora/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.ecommerce.aurora.repositories;
+
+import com.ecommerce.aurora.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
